5d1f58e1bddead0ba3ecc8a94ba82b8809f08165
anusha <anumarripalli@gmail.com>
2025-10-29 18:36:36 +0530

    anu commit

---
+++ anu1.py
@@ -0,0 +1,11 @@
+
+a=10
+b=20
+c=13
+
+if a>=b and a>c:
+    print(a, "is the largest number")
+elif b>=a and b>=c:
+    print(b,"is the largest number")
+else:
+    print(" c is the largest number")
