3a3a23e02b9c5a7f91c0271ee285e7f637126879
anusha <anumarripalli@gmail.com>
2025-10-29 20:54:19 +0530

    final commit

---
+++ ex5.py
@@ -0,0 +1,13 @@
+
+a=10
+b=20
+c=13
+d=20
+
+if a>=b and a>c:
+    print(a, "is the largest number")
+elif b>=a and b>=c:
+    print(b,"is the largest number")
+
+else:
+    print(" c is the largest number")
